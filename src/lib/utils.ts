c1404bbffd962ec37baee7d94717aeecba036766
Calvin <calvinsoares17@gmail.com>
2024-07-28 22:23:44 -0300

    feat: typing color to number

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,7 +13,7 @@ export const colors = [
   "bg-[#4cc9f02a] text-[#4cc9f0] border border-[#4cc9f0bb]",
 ];
 
-export const getColors = (color) => {
+export const getColors = (color: number) => {
   if (color >= 0 && color < colors.length) {
     return colors[color];
   }
