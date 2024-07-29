1bd0741b4fa15372c06445d896a54edd2cf8a874
Calvin <calvinsoares17@gmail.com>
2024-07-28 22:27:28 -0300

    feat: archive with chat componentes

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import ContactsContainer from "./components/contacts-container";
-import EmptyChatContainer from "./components/empty-chat-container";
 import ChatContainer from "./components/chat-container";
+import EmptyChatContainer from "./components/empty-chat-container";
 
 const Chat = () => {
-  const { userInfo } = useAppStore();
+  const { userInfo, selectedChatType } = useAppStore();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,8 +20,11 @@ const Chat = () => {
   return (
     <div className="flex h-[100vh] text-white overflow-hidden">
       <ContactsContainer />
-      {/* <EmptyChatContainer /> */}
-      <ChatContainer />
+      {selectedChatType === undefined ? (
+        <EmptyChatContainer />
+      ) : (
+        <ChatContainer />
+      )}
     </div>
   );
 };
