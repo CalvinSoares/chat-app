f5053238332ac8c09d58b7a4d4e7d3f94e57a224
Calvin <calvinsoares17@gmail.com>
2024-07-28 22:24:38 -0300

    feat: socketProvider for socket.io messages in real time

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "./components/ui/sonner.tsx";
+import { SocketProvider } from "./context/SocketContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <>
-    <App />
-    <Toaster closeButton />
+    <SocketProvider>
+      <App />
+      <Toaster closeButton />
+    </SocketProvider>
   </>,
   //</React.StrictMode>,
 );
